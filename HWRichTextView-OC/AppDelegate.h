515556d6b30c2d7f75937bfa0e3e1cce9d053771
HuangWay <hw758214335@163.com>
2016-01-27 15:18:31 +0800

    Initial Commit

---
+++ HWRichTextView-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HWRichTextView-OC
+//
+//  Created by HuangWay on 16/1/27.
+//  Copyright © 2016年 HuangWay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HWRichTextView-OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWRichTextView-OC
+//
+//  Created by HuangWay on 16/1/27.
+//  Copyright © 2016年 HuangWay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
